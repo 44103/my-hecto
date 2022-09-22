5e9753b2c1f6e225fbeb96fb10f3ab8c18c9b2f6
kenko <kyoshitomi.cursor@gmail.com>
2022-09-23 07:58:58 +0900

    a line viewer

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,9 +1,12 @@
 #![warn(clippy::all, clippy::pedantic)]
+mod document;
 mod editor;
+mod row;
 mod terminal;
-pub use terminal::Terminal;
-
+pub use document::Document;
 use editor::Editor;
+pub use row::Row;
+pub use terminal::Terminal;
 
 fn main() {
     Editor::default().run();
