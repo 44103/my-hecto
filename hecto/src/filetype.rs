bc6ac1336e0f4693c2109bad7b8538bd31cee0cf
kenko <kyoshitomi.cursor@gmail.com>
2022-11-20 17:14:15 +0900

    colorful keywords

--- hecto/src/filetype.rs
+++ hecto/src/filetype.rs
@@ -3,12 +3,14 @@ pub struct FileType {
     hl_ops: HighlightingOptions,
 }
 
-#[derive(Default, Clone, Copy)]
+#[derive(Default)]
 pub struct HighlightingOptions {
     numbers: bool,
     strings: bool,
     characters: bool,
     comments: bool,
+    primary_keywords: Vec<String>,
+    secondary_keywords: Vec<String>,
 }
 
 impl Default for FileType {
@@ -25,8 +27,8 @@ impl FileType {
         self.name.clone()
     }
 
-    pub fn highlighting_options(&self) -> HighlightingOptions {
-        self.hl_ops
+    pub fn highlighting_options(&self) -> &HighlightingOptions {
+        &self.hl_ops
     }
 
     pub fn from(filename: &str) -> Self {
@@ -38,6 +40,24 @@ impl FileType {
                     strings: true,
                     characters: true,
                     comments: true,
+                    primary_keywords: vec![
+                        "as", "break", "const", "continue", "create", "else", "enum", "extern",
+                        "false", "fn", "for", "if", "impl", "in", "let", "loop", "match", "mod",
+                        "move", "put", "pub", "ref", "return", "self", "Self", "static", "super",
+                        "trait", "true", "type", "unsafe", "use", "where", "while", "dyn",
+                        "abstract", "become", "box", "do", "final", "macro", "override", "priv",
+                        "typeof", "unsized", "virtual", "yield", "async", "await", "try",
+                    ]
+                    .iter()
+                    .map(|x| x.to_string())
+                    .collect(),
+                    secondary_keywords: vec![
+                        "bool", "char", "i8", "i16", "i32", "i64", "isize", "u8", "u16", "u32",
+                        "u64", "usize", "f32", "f64",
+                    ]
+                    .iter()
+                    .map(|x| x.to_string())
+                    .collect(),
                 },
             };
         }
@@ -46,19 +66,25 @@ impl FileType {
 }
 
 impl HighlightingOptions {
-    pub fn numbers(self) -> bool {
+    pub fn numbers(&self) -> bool {
         self.numbers
     }
 
-    pub fn strings(self) -> bool {
+    pub fn strings(&self) -> bool {
         self.strings
     }
 
-    pub fn characters(self) -> bool {
+    pub fn characters(&self) -> bool {
         self.characters
     }
 
-    pub fn comments(self) -> bool {
+    pub fn comments(&self) -> bool {
         self.comments
+    }
+    pub fn primary_keywords(&self) -> &Vec<String> {
+        &self.primary_keywords
+    }
+    pub fn secondary_keywords(&self) -> &Vec<String> {
+        &self.secondary_keywords
     }
 }
